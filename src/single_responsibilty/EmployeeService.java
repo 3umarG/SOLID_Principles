cf850d7612c4039b09bdc65f141a164a18571025
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-11-02 23:08:02 +0200

    S.R.P Definition and find Solution Problem Commit

--- src/single_responsibilty/EmployeeService.java
+++ src/single_responsibilty/EmployeeService.java
@@ -1,6 +1,15 @@
 package single_responsibilty;
 
 public class EmployeeService {
+    // This Class Violate Single Responsibility Principle "S.R.P"
+    // Why ???
+    // Because this Class has more than one reason to modify on it ...
+    // EmployeeService should only have methods that response to employee ONLY
+    // But Function Send is another reason to modify this class ...
+    // And it's not one of the main functions of Employee Service
+
+    // SOLUTION : put every method in separate Class and use it when needs ...
+
 
     public void employeeRegistration(int empId){
         // TODO : Add Employee
