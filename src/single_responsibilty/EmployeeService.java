56e0b10e4d9671d743489f6aaf0094b3463ebb21
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-11-02 23:06:22 +0200

    Single Responsibility Principle Problem Commit

---
+++ src/single_responsibilty/EmployeeService.java
@@ -0,0 +1,16 @@
+package single_responsibilty;
+
+public class EmployeeService {
+
+    public void employeeRegistration(int empId){
+        // TODO : Add Employee
+    }
+
+    public void employeeDiscountRate(String empPosition){
+        // TODO : Calculate Employee Discount Rate based on his Position
+    }
+
+    public void sendEmail(){
+        // TODO : Send Email to Employee
+    }
+}
