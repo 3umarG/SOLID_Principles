7c170116cb1aabfb52c4f0c2842bca7a18ed5da2
OmarGomaaFCi <omargomaafcitu@gmail.com>
2022-11-19 01:23:57 +0200

    Dependency Inversion Problem Cause Commit

--- src/dependency_inversion/DeliveryCompany.java
+++ src/dependency_inversion/DeliveryCompany.java
@@ -3,6 +3,13 @@ package dependency_inversion;
 public class DeliveryCompany {
 
     public void sendProduct(Product product){
+        /*
+        * This Structure VIOLATE the Dependency Inversion Principle.
+        * Because The "DeliveryCompany Class" depends on "DeliveryDriver Class".
+        *
+        *
+        * VIOLATION !!! VIOLATION !!! VIOLATION !!! VIOLATION !!!
+         */
         DeliveryDriver driver = new DeliveryDriver();
         driver.deliverProduct(product);
     }
